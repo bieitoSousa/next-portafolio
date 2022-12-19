dcedeca85c1da8d8f54ee83cfdc2eeeda2573c11
bieito.job <bieito.sousa.1991@gmail.com>
2022-12-19 17:01:48 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+   loader : "akamai",
+   path: "",
+  },
 }
 
 module.exports = nextConfig
